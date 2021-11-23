a4e4a843eb701a93b8dfe5c15016c4a2c6768293
Victor Arriola <vicdarrsa@gmail.com>
2021-11-23 21:18:50 +0000

    Inicio

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+// alert("Esto es una prueba...");
+// const dato1 = confirm("Esta es una página WEB que habla sobre la autonomía económica de las mujeres.");
+// console.log(dato1);
+
+alert("¡Bienvenidas mujeres emprendedoras!");
+
+confirm("¿Deseas saber acerca de autonomía económica de las mujeres?");
+
+const dato2 = prompt("Escribe tu nombre:");
+
+// alert(`Hola ${dato2}, te invitamos a ser emprendedora y a desarrollar tu autonomía económica.`);
+
+alert("Hola "+dato2+" te invitamos a ser emprendedora y a desarrollar tu autonomía económica.");
+
